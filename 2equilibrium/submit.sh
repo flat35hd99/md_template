b5f49e8549e817c621240ce48f3fb69ecbca8d37
Wataru Sugiura <flat35hd99@gmail.com>
2022-03-14 20:43:01 +0900

    Integrate run into job script

--- 2equilibrium/submit.sh
+++ 2equilibrium/submit.sh
@@ -3,6 +3,6 @@
 run_start=$1
 run_end=$2
 
-pjsub -N ytva_equ_${run_start}_${run_end} \
+pjsub -N yd_2_${run_start}_${run_end} \
   --bulk --sparam ${run_start}-${run_end} \
   $YTVA_DARK_PROTONATED_PREFIX/2equilibrium/job.sh
